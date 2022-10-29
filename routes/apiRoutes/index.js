6e259961542de62a953038f6035589f8c63b0716
RKH1988 <regettaspearman@gmail.com>
2022-10-29 15:40:00 -0400

    added noteRoute and updated apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,6 @@
 const router = require('express').Router();
-const { notes } = require('../../db/notes');
+const noteRoute = require('./noteRoutes');
 
-router.get('/notes',(req,res)=> {
-    const result = notes;
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-router.post('/notes',(req,res)=>{
-    req.body.id = notes
-})
+router.use(noteRoute);
 
 module.exports = router;
